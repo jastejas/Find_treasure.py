26ba49df17d970f884f1f046434b80a8897d1580
jastejas <144137900+jastejas@users.noreply.github.com>
2023-09-10 13:50:24 +0530

    Create treasure_1.py

---
+++ treasure_1.py
@@ -0,0 +1,11 @@
+print("Welcome to Treasure Island Game.")
+print("Your task is to find the treasure. You have only 1 chance to find it. Good luck!")
+direct = input("You are at a crossroad. Where do you want to go? Right or Left: ")
+if direct == "right":
+        ad = input("There are two houses - Blue and Yellow. Where would you like to go? ")
+        if ad == "blue":
+            print("Congratulations! You have found the treasure. You win!")
+        else:
+            print("You are in the house of fire. Game Over. Sorry, try again.")
+else:
+        print("You fell into a deep hole. Game Over. Sorry, try again.")
