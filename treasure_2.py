93513e3e2e355fefc080bb173d1e054f96d5b690
jastejas <144137900+jastejas@users.noreply.github.com>
2023-09-11 20:38:35 +0530

    Update treasure_2.py

--- treasure_2.py
+++ treasure_2.py
@@ -1,7 +1,7 @@
 print("Welcome to Treasure Island Game.")
 print("Your task is to find the treasure. You have only 1 chance to find it. Good luck!")
 direct = input("You are at a crossroad. Where do you want to go? Right or Left: ")
-if direct=="right":
+if direct=="Right":
     ad=input("There are two houses - Blue and Yellow. Where would you like to go? ")
     if ad=="blue":
         print("Congratulations! You have found the treasure. You win!")
@@ -9,7 +9,7 @@ if direct=="right":
         print("You entered the house of fire. Game Over. Sorry, try again.")
     else:
         print("Invalid choice. Game Over. Sorry, try again.")
-elif direct=="left":
+elif direct=="Left":
     print("You fell into a deep hole. Game Over. Sorry, try again.")
 else:
     print("Invalid choice. Game Over. Sorry, try again.")
