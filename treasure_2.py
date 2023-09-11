631ed0644b57ee4d4d875f6eed920fd6b000499e
jastejas <144137900+jastejas@users.noreply.github.com>
2023-09-11 21:11:39 +0530

    Update treasure_2.py

--- treasure_2.py
+++ treasure_2.py
@@ -3,9 +3,9 @@ print("Your task is to find the treasure. You have only 1 chance to find it. Goo
 direct = input("You are at a crossroad. Where do you want to go? Right or Left: ")
 if direct=="Right":
     ad=input("There are two houses - Blue and Yellow. Where would you like to go? ")
-    if ad=="blue":
+    if ad=="Blue":
         print("Congratulations! You have found the treasure. You win!")
-    elif ad=="yellow":
+    elif ad=="Yellow":
         print("You entered the house of fire. Game Over. Sorry, try again.")
     else:
         print("Invalid choice. Game Over. Sorry, try again.")
